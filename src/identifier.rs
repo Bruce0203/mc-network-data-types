40c4bc719a975c37cdb56ae25242e7178b844bdc
Bruce0203 <BruceFreedy@gmail.com>
2024-03-02 21:04:21 +0900

    update

--- src/identifier.rs
+++ src/identifier.rs
@@ -11,6 +11,16 @@ use super::{
 #[derive(derive_more::Deref)]
 pub struct Identifier(String);
 
+pub trait ToIdentifier {
+    fn to_identifier(&self) -> Identifier;
+}
+
+impl<S: ToString> ToIdentifier for S {
+    fn to_identifier(&self) -> Identifier {
+        Identifier(self.to_string())
+    }
+}
+
 impl Decoder for Identifier {
     fn decode_from_read<R: Read>(reader: &mut R) -> Result<Self> {
         Ok(Identifier(reader.read_var_string::<32767>()?))
